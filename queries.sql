922966d5b2a4d3dd48205550e8097f5bd441adce
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-01-13 15:03:50 -0500

    Add column species to animals table

--- queries.sql
+++ queries.sql
@@ -21,3 +21,7 @@ SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT *FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+--Add a column species of type string to your animals table.
+ALTER TABLE animals ADD COLUMN species VARCHAR;
+
