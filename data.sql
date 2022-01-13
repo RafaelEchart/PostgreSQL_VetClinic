bc73c6611e224842a476cbfc326dfb9fdfb05463
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-01-13 16:28:55 -0500

    CR changes

--- data.sql
+++ data.sql
@@ -168,34 +168,3 @@ INSERT INTO animals(
 	true,
 	17
 );
-
---Transactions
-
-BEGIN;
-UPDATE animals SET species = 'unspecified';
-SELECT * FROM animals;
-ROLLBACK;
-
-
-BEGIN;
-UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
-UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
-COMMIT;
-
-
-BEGIN;
-DELETE FROM animals;
-ROLLBACK;
-
-
-BEGIN;
-DELETE FROM animals
-WHERE date_of_birth > '2022-01-01';
-SAVEPOINT SP1;
-UPDATE animals
-SET weight_kg = weight_kg * -1;
-ROLLBACK TO SP1;
-UPDATE animals
-SET weight_kg = weight_kg * -1
-WHERE weight_kg < 0;
-COMMIT;
--- queries.sql
+++ queries.sql
@@ -39,3 +39,34 @@ SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 
 --What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT species, AVG(escape_attempt) FROM animals WHERE date_of_birth BETWEEN '1990-01-01'  AND '2000-12-31' GROUP BY species;
+
+--Transactions
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
