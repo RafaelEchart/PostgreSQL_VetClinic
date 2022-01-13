c86ccd98c9f310a110fdfea50a0455043d225da3
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-01-13 18:58:53 -0500

    Created Owners and Species tables

--- schema.sql
+++ schema.sql
@@ -1,10 +1,27 @@
+--Create Animals Table
 CREATE TABLE animals (
-	id INT NOT NULL PRIMARY KEY,
-	name VARCHAR(50) NOT NULL,
-	date_of_birth DATE NOT NULL,
-	escape_attempt INT,
-	neutered BOOLEAN NOT NULL,
-	weight_kg NUMERIC NOT NULL
+	id INT GENERATED ALWAYS AS IDENTITY, 
+    name VARCHAR(250), 
+    date_of_birth DATE, 
+    escape_attempts INT, 
+    neutered BOOLEAN, 
+    weight_kg DECIMAL,
+    PRIMARY KEY(id)
+);
+
+--Create Owners Table
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(250),
+    age INT,
+    PRIMARY KEY (id)
+);
+
+--Create Species Table
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250),
+    PRIMARY KEY(id)
 );
 
 --Add a column species of type string to your animals table.
