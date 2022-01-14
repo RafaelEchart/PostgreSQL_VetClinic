529363fe2419374f0ead7992aebb222fdd628826
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-01-13 19:26:08 -0500

    Add foreign key to animals table

--- schema.sql
+++ schema.sql
@@ -26,3 +26,33 @@ CREATE TABLE species (
 
 --Add a column species of type string to your animals table.
 ALTER TABLE animals ADD COLUMN species VARCHAR;
+
+--Remove species column from animals table.
+ALTER TABLE animals DROP COLUMN species;
+
+--Add column species_id which is a foreign key referencing species table
+--Add column owner_id which is a foreign key referencing owner table
+BEGIN;
+ALTER TABLE animals
+ADD COLUMN species_id INT;
+ALTER TABLE animals
+ADD COLUMN owner_id INT;
+COMMIT;
+
+--Connection foreing key species_id with species table (id)
+BEGIN;
+ALTER TABLE animals
+ADD CONSTRAINT species_fk
+FOREIGN KEY (species_id)
+REFERENCES species(id)
+ON DELETE CASCADE;
+COMMIT;
+
+--Connection foreing key owner_id with owner table (id)
+BEGIN;
+ALTER TABLE animals
+ADD CONSTRAINT owner_fk
+FOREIGN KEY (owner_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
+COMMIT;
