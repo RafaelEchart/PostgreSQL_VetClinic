301fcb10bf16945995794d82e082b3adfcc5f3d0
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-01-14 22:23:44 -0500

    Populate Specialization Table}

--- data.sql
+++ data.sql
@@ -193,6 +193,12 @@ VALUES ('William Tatcher', 45, '2000-4-23'),
        ('Stephanie Mendez', 64, '1981-5-4'),
        ('Jack Harkness', 38, '2008-6-8');
 
+-- Populate Specializations table
+INSERT INTO specializations (vets_id, species_id)
+VALUES ((SELECT id FROM vets WHERE name = 'William Tatcher'), (SELECT id FROM species WHERE name = 'Pokemon')),
+       ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Digimon')),
+       ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Pokemon')),
+       ((SELECT id FROM vets WHERE name = 'Jack Harkness'), (SELECT id FROM species WHERE name = 'Digimon'));
 
 --Update species_id in Animals Table
 UPDATE animals
